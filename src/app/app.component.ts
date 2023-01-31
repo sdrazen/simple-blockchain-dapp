748801381f7e400cc7ce71e0b5bcd98ac3840e56
Drazen Skrinjaric <sdrazen@yahoo.com>
2023-01-31 22:51:13 +0100

    Better listening for MetaMask changes, added latest transactions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Renderer2 } from '@angular/core';
+import { Component, OnInit, OnDestroy, Renderer2 } from '@angular/core';
 import { AbiItem } from 'web3-utils'
 import SimpleFundContractJson from '../../build/contracts/SimpleFundContract.json';
 import PriceConsumerV3Json from '../../build/contracts/PriceConsumerV3.json';
@@ -10,7 +10,7 @@ declare let window: any;
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
 
   // General
   web3: Web3;
@@ -30,6 +30,7 @@ export class AppComponent implements OnInit {
   ethFundAmount: string = "0.1";
   funders: string[] = [];
   numberOfLatestFunders: number = 5;
+  numberOfLatestTransactions: number = 5;
 
   // PriceConsumerV3 contract
   priceConsumerV3Object: any;
@@ -40,10 +41,13 @@ export class AppComponent implements OnInit {
   ALERT_TIMEOUT: number = 10000;
   dangerAlertText: string = "";
   successAlertText: string = "";
+  infoAlertText: string = "";
 
   // Transactions
   transactionText: string = "";
   isTransactionInProgress: boolean = false;
+  transactions: any[] = [];
+  numberOfBlocksToCheck: number = 1;
 
   constructor(private renderer: Renderer2) { }
 
@@ -51,6 +55,11 @@ export class AppComponent implements OnInit {
     // Init
     await this.init();
     // Listen for Metamask changes
+    this.listenForMetamaskChanges();
+  }
+
+  ngOnDestroy() {
+    // Un-Listen Metamask changes
     this.listenForMetamaskChanges();
   }
 
@@ -107,8 +116,12 @@ export class AppComponent implements OnInit {
             await this.getEthUsdPrice(8);
             // Get list of funders
             await this.getFunders();
+            // Reset transaction history
+            this.transactions = [];
             // Update interfaceEnabled
             this.interfaceEnabled = true;
+            // Get transaction history
+            this.getTransactionsHistory(this.simpleFundContractAddress, this.numberOfBlocksToCheck);
           } catch (e) {
             // Update interfaceEnabled
             this.interfaceEnabled = false;
@@ -177,30 +190,43 @@ export class AppComponent implements OnInit {
             this.isTransactionInProgress = true;
             // Update transactionText
             this.transactionText = "Waiting for transaction response...";
-            // Begin transaction
-            const transactionResponse = await this.simpleFundContractObject.methods.fund().send({ value: this.web3.utils.toWei(this.ethFundAmount), from: this.accountAddress });
-            // Listen for receipt
-            await this.listenForTransactionMine(transactionResponse, this.web3);
-            // Update SimpleFundContract balance
-            const simpleFundContractBalance = await this.web3.eth.getBalance(this.simpleFundContractAddress);
-            this.simpleFundContractBalance = this.web3.utils.fromWei(simpleFundContractBalance);
-            // Update Account Balance
-            const accountBalance = await this.web3.eth.getBalance(this.accountAddress);
-            this.accountBalance = this.web3.utils.fromWei(accountBalance);
-            // Update list of contract funders
-            await this.getFunders();
-            // Update interfaceEnabled
-            this.interfaceEnabled = true;
-            // Update transactionInProgress
-            this.isTransactionInProgress = false;
+            // Begin transaction (no "await" because we want to listen to transaction stages in the next line)
+            const transactionResponse = this.simpleFundContractObject.methods.fund().send({ value: this.web3.utils.toWei(this.ethFundAmount), from: this.accountAddress });
+            // Listen for transaction stages until receipt is ready
+            try {
+              const receipt = await this.listenForTransactionMine(transactionResponse);
+              // Once receipt was returned successfully, update balances
+              this.transactionText = "Completed!";
+              // Update SimpleFundContract balance
+              const simpleFundContractBalance = await this.web3.eth.getBalance(this.simpleFundContractAddress);
+              this.simpleFundContractBalance = this.web3.utils.fromWei(simpleFundContractBalance);
+              // Update Account Balance
+              const accountBalance = await this.web3.eth.getBalance(this.accountAddress);
+              this.accountBalance = this.web3.utils.fromWei(accountBalance);
+              // Update list of contract funders
+              await this.getFunders();
+              // Update interfaceEnabled
+              this.interfaceEnabled = true;
+              // Update transactionInProgress
+              this.isTransactionInProgress = false;
+              // Push new transaction to array (to the first position because we want array to stay reversed)
+              this.transactions.unshift({ hash: receipt.transactionHash, from: receipt.from, to: receipt.to, value: this.web3.utils.toWei(this.ethFundAmount) });
+              // Remove last array element if needed
+              if (this.transactions.length > this.numberOfLatestTransactions) {
+                this.transactions = this.transactions.slice(0, this.numberOfLatestTransactions);
+              }
+              // Alert Success
+              this.showAlertSuccess("Transaction successfully completed with hash " + receipt.transactionHash);
+              // Show and hide transaction text
+              setTimeout(() => {
+                this.transactionText = "";
+              }, this.ALERT_TIMEOUT);
+            }
+            catch (e) {
+              this.handleTransactionError(e);
+            }
           } catch (e) {
-            // Update transactionInProgress
-            this.isTransactionInProgress = false;
-            // Reset transaction text
-            this.transactionText = "";
-            // Alert Danger
-            // this.showAlertDanger("There was a problem with funding SimpleFundContract contract.")
-            this.showAlertDanger(e.message);
+            this.handleTransactionError(e);
           }
         }
       } catch (e) {
@@ -210,40 +236,81 @@ export class AppComponent implements OnInit {
     }
   }
 
-  listenForTransactionMine(transactionResponse, provider: Web3) {
-    this.transactionText = "Mining...";
-    return new Promise<void>((resolve, reject) => {
+  async getTransactionsHistory(address: string, numberOfBlocks: number = 1) {
+
+    let currentBlockNumber = await this.web3.eth.getBlockNumber();
+    let startBlockNumber = currentBlockNumber - numberOfBlocks + 1;
+
+    for (let blockNumber = startBlockNumber; blockNumber <= currentBlockNumber; blockNumber++) {
+      //Show transaction text
+      this.transactionText = `Checking block ${blockNumber} (${blockNumber - startBlockNumber + 1} / ${numberOfBlocks})`;
+      let block = await this.web3.eth.getBlock(blockNumber);
+      if (block && block.transactions) {
+        for (let txHash of block.transactions) {
+          let tx = await this.web3.eth.getTransaction(txHash);
+          if (tx && tx.from && tx.to && tx.value && address.toLowerCase() == tx.to.toLowerCase()) {
+            this.transactions.push({ hash: txHash, from: tx.from, to: tx.to, value: tx.value });
+          }
+        }
+      }
+    }
+
+    // Hide transaction text
+    this.transactionText = "";
+
+    // Get only needed number of transactions and reverse array
+    this.transactions = this.transactions.slice((-1) * this.numberOfLatestTransactions).reverse();
+
+  }
+
+  listenForTransactionMine(transactionResponse): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
       try {
-        provider.eth.getTransactionReceipt(transactionResponse.transactionHash)
-          .then(() => {
-            this.transactionText = "Completed!";
-            // Alert Success
-            this.showAlertSuccess("Transaction successfully completed!");
-            setTimeout(() => {
-              this.transactionText = "";
-            }, this.ALERT_TIMEOUT);
-            resolve()
-          })
+        transactionResponse
+          .once('receipt', function (receipt) { resolve(receipt); })
+          .on('error', function (error) { reject(error); })
       } catch (error) {
-        this.transactionText = error;
-        setTimeout(() => {
-          this.transactionText = "";
-        }, this.ALERT_TIMEOUT);
-        reject();
+        reject(error);
       }
     })
   }
 
   listenForMetamaskChanges() {
+
+    // If network change is detected, there is no need to check for account change also because
+    // init() will take care of everything anyways. Only if network is same as before we can
+    // check if account is changed
+
     this.renderer.listen('window', 'focus', event => {
-      if (!this.isTransactionInProgress) {
-        this.focusChanged = true;
-        this.interfaceEnabled = false;
-        this.init()
-          .then(() => { this.focusChanged = false })
-          .catch(() => { this.focusChanged = false; this.interfaceEnabled = false })
-      }
+
+      this.web3.eth.net.getId()
+        .then((netId) => {
+          if (netId != this.netId) {
+            if (!this.isTransactionInProgress) {
+              this.focusChanged = true;
+              this.interfaceEnabled = false;
+              this.init()
+                .then(() => { this.focusChanged = false; this.showAlertInfo("Selected network changed in MetaMask!"); })
+                .catch(() => { this.focusChanged = false; this.interfaceEnabled = false })
+            }
+          } else {
+            this.web3.eth.getAccounts()
+              .then((accounts) => {
+                if (accounts[0] != this.accountAddress) {
+                  if (!this.isTransactionInProgress) {
+                    this.focusChanged = true;
+                    this.interfaceEnabled = false;
+                    this.init()
+                      .then(() => { this.focusChanged = false; this.showAlertInfo("Selected account changed in MetaMask!"); })
+                      .catch(() => { this.focusChanged = false; this.interfaceEnabled = false })
+                  }
+                }
+              });
+          }
+        });
+
     });
+
   }
 
   resetInterface() {
@@ -266,6 +333,32 @@ export class AppComponent implements OnInit {
     setTimeout(() => {
       this.successAlertText = "";
     }, this.ALERT_TIMEOUT);
+  }
+
+  showAlertInfo(text: string) {
+    this.infoAlertText = text;
+    setTimeout(() => {
+      this.infoAlertText = "";
+    }, this.ALERT_TIMEOUT);
+  }
+
+  handleTransactionError(e: any) {
+    // Update transactionText
+    this.transactionText = "Error!";
+    // Show and hide transaction text
+    setTimeout(() => {
+      this.transactionText = "";
+    }, this.ALERT_TIMEOUT);
+    // Update transactionInProgress
+    this.isTransactionInProgress = false;
+    // Update interfaceEnabled
+    this.interfaceEnabled = true;
+    // Alert Danger
+    this.showAlertDanger(e.message);
+  }
+
+  convertFromWei(value: string): string {
+    return this.web3.utils.fromWei(value);
   }
 
 }
